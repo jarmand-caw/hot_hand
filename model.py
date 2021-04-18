8244171bc6e3b0b543d5e186fc5bd5942aa41974
JackArmand <jack.armand2@gmail.com>
2021-04-18 14:53:02 -0400

    initial commit

---
+++ model.py
@@ -0,0 +1,189 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from utils import Utils
+from utils import conf
+from sklearn.metrics import accuracy_score
+from copy import deepcopy
+
+
+utils = Utils(conf)
+utils.prep_dataset()
+utils.create_torch_loaders()
+
+class ShotModel(nn.Module):
+    def __init__(self, config):
+        super(ShotModel, self).__init__()
+
+        self.embedding_dims = config['embedding_dims']
+        self.w_embedding_dim = self.embedding_dims['W']
+        self.period_embedding_dim = self.embedding_dims['PERIOD']
+        self.pts_type_embedding_dim = self.embedding_dims['PTS_TYPE']
+        self.defender_embedding_dim = self.embedding_dims['CLOSEST_DEFENDER_PLAYER_ID']
+        self.player_embedding_dim = self.embedding_dims['player_id']
+
+        self.w_embedding = nn.Embedding(len(utils.w_map), self.w_embedding_dim)
+        self.period_embedding = nn.Embedding(len(utils.period_map), self.period_embedding_dim)
+        self.pts_type_embedding = nn.Embedding(len(utils.pts_type_map), self.pts_type_embedding_dim)
+        self.defender_embedding = nn.Embedding(len(utils.defender_map), self.defender_embedding_dim)
+        self.player_embedding = nn.Embedding(len(utils.player_map), self.player_embedding_dim)
+
+        self.cont_layer_dims = config['cont_layers']
+        self.cont_layer_dims.insert(0, len(utils.cont_cols))
+        self.cont_layers = []
+        for idx in np.arange(len(self.cont_layer_dims)-1):
+            in_features = self.cont_layer_dims[idx]
+            out_features = self.cont_layer_dims[idx+1]
+            self.cont_layers.append(nn.Linear(in_features, out_features))
+            self.cont_layers.append(nn.ReLU())
+        self.cont_forward = nn.Sequential(*self.cont_layers)
+
+        self.cat_layer_dims = config['cat_layers']
+        self.cat_layer_dims.insert(0, self.w_embedding_dim+self.period_embedding_dim+self.pts_type_embedding_dim+self.defender_embedding_dim)
+        self.cat_layers = []
+        for idx in np.arange(len(self.cat_layer_dims)-1):
+            in_features = self.cat_layer_dims[idx]
+            out_features = self.cat_layer_dims[idx+1]
+            self.cat_layers.append(nn.Linear(in_features, out_features))
+            self.cat_layers.append(nn.ReLU())
+        self.cat_forward = nn.Sequential(*self.cat_layers)
+
+        self.deep_layer_dims = config['deep_layers']
+        self.deep_layer_dims.insert(0, self.player_embedding_dim*3)
+        self.deep_layers = []
+        for idx in np.arange(len(self.deep_layer_dims)-1):
+            in_features = self.deep_layer_dims[idx]
+            out_features = self.deep_layer_dims[idx+1]
+            self.deep_layers.append(nn.Linear(in_features, out_features))
+            self.deep_layers.append(nn.ReLU())
+
+        self.deep_forward = nn.Sequential(*self.deep_layers)
+
+        self.affine_output = nn.Linear(self.deep_layer_dims[-1], 1)
+
+    def forward(self, variables):
+
+        w = variables[:, 0].long()
+        period = variables[:, 1].long()
+        pts = variables[:, 2].long()
+        defender = variables[:, 3].long()
+        player = variables[:, 4].long()
+
+        cont = variables[:, 5:]
+
+        w_emb = self.w_embedding(w)
+        per_emb = self.period_embedding(period)
+        pts_emb = self.pts_type_embedding(pts)
+        def_emb = self.defender_embedding(defender)
+        player_emb = self.player_embedding(player)
+
+        cat_emb = torch.cat((w_emb, per_emb, pts_emb, def_emb), 1)
+        cat_out = self.cat_forward(cat_emb)
+        cont_out = self.cont_forward(cont)
+
+        out = torch.cat((cat_out, cont_out), 1)
+
+        player_effect = out*player_emb
+        total_out = torch.cat((player_effect, out, player_emb), 1)
+
+        out = self.deep_forward(total_out)
+        affine = self.affine_output(out)
+        return affine
+
+class Engine:
+    def __init__(self, model, config):
+        self.device = config['device']
+        if self.device == 'cuda':
+            self.model = model.cuda()
+        else:
+            self.model = model
+        self.num_epochs = config['num_epochs']
+        self.learning_rate = config['learning_rate']
+        self.optim_name = config['optim']
+        self.weight_decay = config['weight_decay']
+        self.momentum = config['momentum']
+        self.criterion = config['criterion']
+        self.sigmoid = nn.Sigmoid()
+
+    def create_optimizer(self):
+        if self.optim_name == 'SGD':
+            self.optimizer = optim.SGD(self.model.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay, momentum=self.momentum)
+        elif self.optim_name == 'Adam':
+            self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        else:
+            raise TypeError('Optimizer not supported yet. Please pick SGD or Adam')
+
+    def train_one_batch(self, x, y):
+        if self.device == 'cuda':
+            x = x.cuda()
+            y = y.cuda()
+        self.optimizer.zero_grad()
+        outputs = self.model(x)
+        loss = self.criterion(outputs, y)
+        loss.backward()
+        self.optimizer.step()
+        preds = self.sigmoid(outputs.detach()) > 0.5
+        return loss.item(), list(preds.numpy()), list(y.detach().numpy())
+
+    def train_one_epoch(self, epoch_num):
+
+        epoch_loss = 0
+        all_preds = []
+        all_labels = []
+        for x,y in utils.train_loader:
+            if self.device == 'cuda':
+                x = x.cuda()
+                y = y.cuda()
+            l, preds, labels = self.train_one_batch(x, y)
+            epoch_loss += l
+            all_preds += preds
+            all_labels += labels
+
+        print('Train Scores:')
+        print('Epoch:', epoch_num, 'Loss:', epoch_loss, 'Accuracy:', accuracy_score(all_labels, all_preds))
+
+    def test_one_epoch(self, epoch_num):
+
+        epoch_loss = 0
+        all_preds = []
+        all_labels = []
+        for x, y in utils.test_loader:
+            outputs = self.model(x)
+            preds = self.sigmoid(outputs.detach()) > 0.5
+            preds = list(preds.numpy())
+            l = self.criterion(outputs, y).item()
+            labels = list(y.detach().numpy())
+            epoch_loss += l
+            all_preds += preds
+            all_labels += labels
+
+        print('Test Scores:')
+        print('Epoch:', epoch_num, 'Loss:', epoch_loss, 'Accuracy:', accuracy_score(all_labels, all_preds))
+        return epoch_loss
+
+    def train_to_completion(self):
+        self.create_optimizer()
+        best_loss = 1e10
+        self.best_model = None
+        tolerance = 10
+        breaking = 0
+        for epoch in range(self.num_epochs):
+            self.train_one_epoch(epoch)
+            l = self.test_one_epoch(epoch)
+            if l < best_loss:
+                best_loss = l
+                self.best_model = deepcopy(self.model.state_dict())
+                breaking = 0
+            else:
+                breaking += 1
+                if breaking >= tolerance:
+                    break
+
+        print('Training stopped at epoch {}'.format(epoch))
+        print('Best testing loss = {}'.format(best_loss))
+
+
+model = ShotModel(conf)
+engine = Engine(model, conf)
+engine.train_to_completion()
---
+++ utils.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+from torch.utils.data import TensorDataset, DataLoader
+
+possible_cont_cols = ['FINAL_MARGIN', 'GAME_CLOCK', 'SHOT_CLOCK', 'DRIBBLES', 'TOUCH_TIME',
+                     'SHOT_DIST', 'CLOSE_DEF_DIST']
+possible_cat_cols = ['W', 'PERIOD', 'PTS_TYPE', 'CLOSEST_DEFENDER_PLAYER_ID', 'player_id']
+
+conf = {
+    'file_path': '/Users/jackarmand/Documents/hot-hand/shot_logs.csv',
+    'cont_cols': possible_cont_cols,
+    'cat_cols': possible_cat_cols,
+    'embedding_dims': {'W': 5, 'PERIOD':25, 'PTS_TYPE': 25, 'CLOSEST_DEFENDER_PLAYER_ID':50, 'player_id': 300},
+    'cat_layers': [500, 250, 50],
+    'cont_layers': [500, 250, 250],
+    'deep_layers': [750, 500, 500, 50],
+    'num_epochs': 30,
+    'learning_rate': 1e-2,
+    'optim': 'SGD',
+    'weight_decay': 0,
+    'momentum': 0.9,
+    'criterion': nn.BCEWithLogitsLoss(),
+    'device': 'cpu'
+}
+
+class Utils:
+    def __init__(self, config):
+        self.file_path = config['file_path']
+        self.cat_cols = config['cat_cols']
+        self.cont_cols = config['cont_cols']
+
+    def prep_dataset(self):
+        shots_df = pd.read_csv(self.file_path)
+
+        # Find players only with over 500 shots taken on the season
+        player_shot_counts = pd.DataFrame(shots_df.groupby('player_id').count()['GAME_ID'])
+        players_over_500 = list(player_shot_counts.loc[player_shot_counts['GAME_ID']>500].index)
+        shots_df = shots_df.loc[shots_df['player_id'].isin(players_over_500)]
+
+        # Perform feature engineering on time options
+        def convert_game_clock_to_seconds(clock_string):
+            splits = clock_string.split(':')
+            splits = [int(x) for x in splits]
+            return splits[0]*60 + splits[1]
+
+        shots_df['GAME_CLOCK'] = shots_df['GAME_CLOCK'].apply(convert_game_clock_to_seconds)
+
+        time_chunks = []
+        crunch_time = []
+        for row_idx in range(len(shots_df)):
+            row = shots_df.iloc[row_idx]
+            game_clock = row['GAME_CLOCK']
+            shot_clock = row['SHOT_CLOCK']
+            if game_clock < 3:
+                time_chunks.append(1)
+            elif shot_clock < 3:
+                time_chunks.append(1)
+            else:
+                time_chunks.append(0)
+
+            period = row['PERIOD']
+            if period == 4:
+                if game_clock < 240:
+                    crunch_time.append(1)
+                else:
+                    crunch_time.append(0)
+            else:
+                crunch_time.append(0)
+
+        shots_df['TIME_CHUCK'] = time_chunks
+        shots_df['CRUNCH_TIME'] = crunch_time
+
+        shots_df['TOTAL_ELAPSED_TIME'] = (shots_df['PERIOD']-1) * 12 * 60 + shots_df['GAME_CLOCK']
+
+        # Transformations to categorical columns to prepare for modeling
+
+        self.w_map = {'W':1, 'L':0}
+        shots_df['W'] = shots_df['W'].map(self.w_map)
+
+        period_options = list(set(list(shots_df['PERIOD'])))
+        self.period_map = dict(zip(period_options, list(np.arange(len(period_options)))))
+        shots_df['PERIOD'] = shots_df['PERIOD'].map(self.period_map)
+        self.id = shots_df.loc[~shots_df['PERIOD'].isin(list(self.period_map.values()))]
+
+        self.pts_type_map = {2:0, 3:1}
+        shots_df['PTS_TYPE'] = shots_df['PTS_TYPE'].map(self.pts_type_map)
+
+        defender_list = list(set(list(shots_df['CLOSEST_DEFENDER_PLAYER_ID'])))
+        self.defender_map = dict(zip(defender_list, list(np.arange(len(defender_list)))))
+
+        self.player_list = players_over_500.copy()
+        self.player_map = dict(zip(self.player_list, list(np.arange(len(self.player_list)))))
+
+        shots_df['CLOSEST_DEFENDER_PLAYER_ID'] = shots_df['CLOSEST_DEFENDER_PLAYER_ID'].map(self.defender_map)
+        shots_df['player_id'] = shots_df['player_id'].map(self.player_map)
+
+        # Creating y values with 1s and 0s for made or missed
+        self.y_col = ['SHOT_RESULT']
+        result_map = {'made':1, 'missed':0}
+        shots_df['SHOT_RESULT'] = shots_df['SHOT_RESULT'].map(result_map)
+
+        self.possible_cont_cols = ['FINAL_MARGIN', 'GAME_CLOCK', 'SHOT_CLOCK', 'DRIBBLES', 'TOUCH_TIME',
+                     'SHOT_DIST', 'CLOSE_DEF_DIST']
+
+        self.possible_cat_cols = ['W', 'PERIOD', 'PTS_TYPE', 'CLOSEST_DEFENDER_PLAYER_ID', 'player_id']
+
+        shots_df.reset_index(inplace=True, drop=True)
+        shots_df = shots_df[self.cat_cols+self.cont_cols+self.y_col]
+        shots_df.dropna(axis='rows', how='any', inplace=True)
+        for col in self.cont_cols:
+            shots_df[col] = (shots_df[col]-shots_df[col].mean())/shots_df[col].std()
+
+        self.shots_df = shots_df
+
+    def create_torch_loaders(self):
+
+        # Want training and test sets to be seperated by player, so that for each player's shots specifically 2/3 are
+        # in training and 1/3 is in testing
+        np.random.seed(0)
+
+        self.total_train_set = []
+        self.total_test_set = []
+
+        for player in np.arange(len(self.player_list)):
+            player_df = self.shots_df.loc[self.shots_df['player_id'] == player]
+            player_idx = list(player_df.index)
+
+            test_set = np.random.choice(player_idx, replace=False, size=int(len(player_idx)/3))
+            train_set = set(player_idx) - set(test_set)
+            train_set = list(train_set)
+            test_set = list(test_set)
+
+            self.total_train_set += train_set
+            self.total_test_set += test_set
+
+        train_data = self.shots_df.loc[self.total_train_set]
+        test_data = self.shots_df.loc[self.total_test_set]
+
+        x_train = train_data[self.cat_cols + self.cont_cols].values
+        y_train = train_data[self.y_col].values
+
+        x_test = test_data[self.cat_cols + self.cont_cols].values
+        y_test = test_data[self.y_col].values
+
+        train_dataset = TensorDataset(torch.Tensor(x_train), torch.Tensor(y_train))
+        test_dataset = TensorDataset(torch.Tensor(x_test), torch.Tensor(y_test))
+
+        train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=len(x_test), shuffle=False)
+
+        self.train_loader = train_loader
+        self.test_loader = test_loader
